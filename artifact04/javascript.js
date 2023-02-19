43eabc681ad2638b69f1659e00370ee5e0d35b27
Alex125142 <121984582+Alex125142@users.noreply.github.com>
2023-02-18 23:40:46 -0800

    Update javascript.js

--- artifact04/javascript.js
+++ artifact04/javascript.js
@@ -1,17 +1,17 @@
 function validateEmail() {
     var x = document.getElementById("email").value;
-    var atPos = x.indexOf("@");
-    var dotPos = x.lastIndexOf(".");
-    if (atpos < 1 || dotPos < atPos + 2 || dotPos + 2 >= x.length) {
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
         return false;
     } else {
         return true;
     }
 }
-function validateForm(){
+
+function validateForm() {
     var firstName = document.getElementById("firstname").value;
     var lastName = document.getElementById("lastname").value;
-    var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -20,7 +20,7 @@ function validateForm(){
     var country = document.getElementById("country").value;
     var zip = document.getElementById("zip").value;
     var errorMessage = "";
-
+  
     var validFirstName = false;
     var validLastName = false;
     var validEmail = false;
@@ -31,7 +31,7 @@ function validateForm(){
     var validCity = false;
     var validCountry = false;
     var validZip = false;
-   if (firstName.length <= 20 && firstName.length > 0) {
+    if (firstName.length <= 20 && firstName.length > 0) {
         validFirstName = true;
     } else {
         errorMessage += "<p>Invalid First Name</p>";
@@ -46,11 +46,11 @@ function validateForm(){
     } else {
         errorMessage += "<p>Invalid Email</p>";
     }
-  if (isNaN(phone) || phone.lenght >15 || phone===null || phone===""){
-    validPhone = true;
-  } else{
-    errorMessage += "<p>Invalid Phone</p>";
-  }
+    if (phone.length <= 15 && phone.length > 0 && !isNaN(phone)) {
+        validPhone = true;
+    } else {
+        errorMessage += "<p>Invalid Phone #</p>";
+    }
   if (username.length <= 12 && username.length > 0) {
         validUsername = true;
     } else {
@@ -61,7 +61,7 @@ function validateForm(){
   } else{
     errorMessage += "<p>Invalid Password</p>";
   }
-  if(address > 0){
+  if(address.length > 0){
     validAddress = true;
   }else {
     errorMessage += "<p>Invalid Address</p>";
@@ -77,8 +77,10 @@ function validateForm(){
     } else {
   errorMessage += "<p>Invalid Zip</p>";
   }
-}
+    }
+    else {
+        validZip = true;
+    }
     document.getElementById("error").innerHTML = errorMessage;
     return (validFirstName && validLastName && validEmail && validPhone && validUsername && validPassword && validAddress && validCity && validCountry && validZip);
 }
-
